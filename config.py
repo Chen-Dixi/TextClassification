54d754f259ce37cd0bc05725a6054b02d520bc8f
Chen-Dixi <cdxcqu2014@163.com>
2019-05-24 00:00:40 +0800

    start

---
+++ config.py
@@ -0,0 +1,18 @@
+import yaml
+import easydict
+from os.path import join
+
+import argparse
+parser = argparse.ArgumentParser(description='Code for *Universal Domain Adaptation*',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--config', type=str, default='config.yaml', help='/path/to/config/file')
+
+args = parser.parse_args()
+
+config_file = args.config
+
+args = yaml.load(open(config_file))
+
+args = easydict.EasyDict(args)
+
+
